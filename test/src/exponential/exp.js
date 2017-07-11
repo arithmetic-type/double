4dea9211b7bb8dcf00213aa6b527fc8b4466c374
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-07-11 21:57:26 +0200

    up deps, rebuild and test exp

---
+++ test/src/exponential/exp.js
@@ -0,0 +1,21 @@
+import test from 'ava' ;
+import * as number from '../../../src' ;
+
+test( "exp", t => {
+
+	const one = function ( a ) {
+		t.deepEqual( number.exp( a ), Math.exp( a ) , `e^${a}` );
+	} ;
+
+	const n = 10;
+
+	for ( let i = 0 ; i < n ; ++i ) {
+		const a = Math.random();
+		one( a );
+	}
+
+	one(  Infinity );
+	one(         0 );
+	one( -Infinity );
+
+} );
